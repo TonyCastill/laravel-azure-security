8fa1b89cbd2d4e9f04c796e0870dc6893a7e0437
tony <starreach33@gmail.com>
2025-12-02 08:19:58 -0600

    Added Addition test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,4 +13,13 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function test_addition_result(): void
+    {
+        $controller = new \App\Http\Controllers\OperationsController;
+        $result = $controller->addition(2, 3);
+        $this->assertIsInt($result);
+        $this->assertNotNull($result);
+        $this->assertEquals(5, $result);
+    }
 }
