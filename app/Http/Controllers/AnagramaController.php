fa710ce382cf162429f666a2a1b51b28b4bed808
vargascelerin <vargascelerin@gmail.com>
2025-12-06 20:24:12 -0600

    HotFix 3, correción de estilos pint

--- app/Http/Controllers/AnagramaController.php
+++ app/Http/Controllers/AnagramaController.php
@@ -4,18 +4,19 @@ namespace App\Http\Controllers;
 
 class AnagramaController extends Controller
 {
-/**
- * @return array{palabra1: string, palabra2: string, son_anagramas: bool}
- */
+    /**
+     * @return array{palabra1: string, palabra2: string, son_anagramas: bool}
+     */
     public static function sonAnagramas(string $str1, string $str2): array
     {
         $arr1 = str_split(strtolower($str1));
         $arr2 = str_split(strtolower($str2));
-        
+
         sort($arr1);
         sort($arr2);
-        
-        $sonAnagramas = $arr1 === $arr2;       
+
+        $sonAnagramas = $arr1 === $arr2;
+
         return [
             'palabra1' => $str1,
             'palabra2' => $str2,
--- tests/Unit/AnagramaTest.php
+++ tests/Unit/AnagramaTest.php
@@ -2,23 +2,23 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
 use App\Http\Controllers\AnagramaController as Anagrama;
+use Tests\TestCase;
 
 class AnagramaTest extends TestCase
 {
     public function test_son_anagramas()
     {
         $resultado1 = Anagrama::sonAnagramas('listen', 'silent');
-        echo "\n'{$resultado1['palabra1']}' y '{$resultado1['palabra2']}' son anagramas: " . ($resultado1['son_anagramas'] ? 'Sí' : 'No') . "\n";
+        echo "\n'{$resultado1['palabra1']}' y '{$resultado1['palabra2']}' son anagramas: ".($resultado1['son_anagramas'] ? 'Sí' : 'No')."\n";
         $this->assertTrue($resultado1['son_anagramas']);
-        
+
         $resultado2 = Anagrama::sonAnagramas('amor', 'roma');
-        echo "'{$resultado2['palabra1']}' y '{$resultado2['palabra2']}' son anagramas: " . ($resultado2['son_anagramas'] ? 'Sí' : 'No') . "\n";
+        echo "'{$resultado2['palabra1']}' y '{$resultado2['palabra2']}' son anagramas: ".($resultado2['son_anagramas'] ? 'Sí' : 'No')."\n";
         $this->assertTrue($resultado2['son_anagramas']);
-        
+
         $resultado3 = Anagrama::sonAnagramas('hello', 'world');
-        echo "'{$resultado3['palabra1']}' y '{$resultado3['palabra2']}' son anagramas: " . ($resultado3['son_anagramas'] ? 'Sí' : 'No') . "\n";
+        echo "'{$resultado3['palabra1']}' y '{$resultado3['palabra2']}' son anagramas: ".($resultado3['son_anagramas'] ? 'Sí' : 'No')."\n";
         $this->assertFalse($resultado3['son_anagramas']);
     }
 }
